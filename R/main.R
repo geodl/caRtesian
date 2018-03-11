d3e4efee9e112ca41ec81ac69332e18721eef534
Ryan Porteous <rp10@hw.ac.uk>
2018-03-11 16:02:46 +0000

    Updated main to reflect recent changes

--- R/main.R
+++ R/main.R
@@ -4,15 +4,17 @@ cgp <- function(dataset,
 
   dataset <- read.csv("./data/x_squared_minus_y.csv")
 
+  functionSet <<- functionSet()
+
   ##Placeholders - these need to be determined either through headers or
   ##a scripted solution should be provided
-  outputSize <<- 1
-  inputSize <<- ncol(dataset) - outputSize
+  outputSize <<- calculateOutputSize(dataset)
+  inputSize <<- calculateInputSize(dataset, outputSize)
 
   popsize <- 5
 
   population <- initPopulation(popsize)
 
   #Return results to top level
-  #return()
+  return(population)
 }
