60cb63a117cf0b9f398b61c67f5ea9efaf2b46dd
Ryan Porteous <rp10@hw.ac.uk>
2018-04-05 02:24:33 +0100

    Added fitness functions

--- R/fitness.R
+++ R/fitness.R
@@ -4,15 +4,13 @@
 #'
 #' @param population the list of elements to be evaluated
 #' @param dataset the dataset used to evaluate the element against
+#' @param fitnessFunction the fitness function to use
 #'
 #' @return
 #' @export
 #'
 #' @examples
-calculatePopFitness <- function(population, dataset) {
-
-  population <- pop
-  dataset <- read.csv("./data/x_squared_minus_y.csv")
+calculatePopFitness <- function(population, dataset, fitnessFunction) {
 
   for(i in population) {
 
@@ -24,13 +22,51 @@ calculatePopFitness <- function(population, dataset) {
   return(population)
 }
 
-calculateFitness <- function(solution, dataset) {
+calculateFitness <- function(solution, dataset, fitnessFunction) {
 
 
   fitness <- as.numeric(0)
 
+#' rmse
+#'
+#' Calculates the Root Mean Squared Error on a set of predicted and actual
+#' values. RMSE gives a large weight to large errors and should be used where
+#' large errors are undesirable.
+#'
+#' @param data a dataset containing predicted and actual values
+#'
+#' @return the rmse value
+#' @export
+rmse <- function(data) {
 
-  #get the first row in the dataset
-  #pass this as input into the inputNodes
-  #so I set the solution$inputNodes values from 1:inputSize as the
+  residuals <- vector(mode = "numeric", length = nrow(data))
+  for(i in data) {
+    #Substract actual value from predicted value and square result
+    residuals[i] <- (data$predicted - data$actual)^2
+  }
+
+  #Square root the mean of the residuals
+  return (sqrt(mean(residuals)))
+}
+
+#' mae
+#'
+#' Calculates the Mean Absolute Error on a set of predicted and actual values.
+#' MAE produces a clear indicator of the average absolute difference between
+#' predicted and actual values.
+#'
+#' @param data a dataset containing predicted and actual values
+#'
+#' @return the mae value
+#' @export
+mae <- function(data) {
+
+  results <- vector(mode = "numeric", length = nrow(data))
+  for(i in data) {
+    #Subtract the predicted value from actual value and take the absolute value
+    residual[i] <- abs(data$actual - data$predicted)
+  }
+
+  #Mean of the results
+  return(mean(results))
 }
--- R/main.R
+++ R/main.R
@@ -6,6 +6,7 @@
 #' @param model a model specifying the output and input fields of the dataset
 #' @param functionSet the functions that can be used in the function nodes
 #' @param stopCondition the stopping criteria of the evolutionary process
+#' @param fitnessFunction the fitness function to use
 #' @param rowsFuncNodes the number of rows to use in the function node structure
 #' @param colsFuncNodes the number of columns to use in the function node structure
 #' @param levelsBack the number of columns back that a function node can access
@@ -15,6 +16,7 @@
 #' @export
 cgp <- function(dataset, model, functionSet,
                 stopCondition = timeCondition(5 * 60),
+                maxGenerations, fitnessFunction,
                 rowsFuncNodes, colsFuncNodes, levelsBack,
                 popSize) {
 
