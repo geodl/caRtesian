1d7a1bd1719c885ab958dbeffda89ca46f7fe231
Ryan Porteous <rp10@hw.ac.uk>
2018-04-06 01:21:21 +0100

    Added selectionMethod stuff
    Problems are discussed in problems document

--- R/main.R
+++ R/main.R
@@ -7,6 +7,7 @@
 #' @param functionSet the functions that can be used in the function nodes
 #' @param maxGenerations the maximum generations of the evolutionary process
 #' @param fitnessFunction the fitness function to use
+#' @param selectionMethod the function and parameters to use for selection
 #' @param rowsFuncNodes the number of rows to use in the function node structure
 #' @param colsFuncNodes the number of columns to use in the function node structure
 #' @param levelsBack the number of columns back that a function node can access
@@ -16,6 +17,8 @@
 #' @export
 cgp <- function(dataset, model, functionSet,
                 maxGenerations, fitnessFunction,
+                selectionMethod = list(func = muLambda,
+                                       args = c(population = NA, 4)),
                 rowsFuncNodes, colsFuncNodes, levelsBack,
                 popSize) {
 
@@ -32,6 +35,26 @@ cgp <- function(dataset, model, functionSet,
 
   #Calculate the fitness values of the population
   population <- calculatePopFitness(population, dataset, fitnessFunction)
+
+
+
+  #Setup variables required for evolutionary process
+  selection <- selectionMethod$func
+  args <- selectionMethod$args
+  population <- sortPopulation(population)
+  currGeneration <- 0
+  solutionFound <- FALSE
+
+  #Run evolution
+  while(currGeneration < maxGeneration && !isTRUE(solutionFound)) {
+    population <- selection(population, args[1])
+
+    population <- calculatePopFitness(population, dataset, fitnessFunction)
+
+    solutionFound <- checkSolutionFound(population)
+    currGeneration <- currGeneration + 1
+  }
+
 
   #Return results to top level
   return(population)
