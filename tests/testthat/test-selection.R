79dfed2a040f962fb5d98d1ab37750bfc23894e3
Ryan Porteous <rp10@hw.ac.uk>
2018-04-10 04:18:51 +0100

    Started unit testing for selection.R

---
+++ tests/testthat/test-selection.R
@@ -0,0 +1,13 @@
+library(caRtesian)
+context("selection")
+
+test_that("muLambdaStrategy returns the correct number of results", {
+
+  population <- initPopulation(5, mathOpSet(), inputSize = 2,
+                               outputSize = 1, rowsFuncNodes = 5,
+                               colsFuncNodes = 5, levelsBack = 2)
+
+
+
+
+})
