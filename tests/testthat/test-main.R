3ca8548788fcc9e44f104f0761be551b7e549287
Ryan Porteous <rp10@hw.ac.uk>
2018-04-13 21:33:32 +0100

    Added more unit tests

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -1,2 +1,14 @@
 library(caRtesian)
 context("main")
+
+test_that("cgp returns a list with the correct structure", {
+
+  pop <- cgp(dataset = read.csv("./data/x_squared_minus_y.csv"),
+             model = output ~ x + y,
+             maxGenerations = 1,
+             rowsFuncNodes = 5,
+             colsFuncNodes = 5,
+             levelsBack = 2,
+             popSize = 5)
+
+})
--- tests/testthat/test-selection.R
+++ tests/testthat/test-selection.R
@@ -7,8 +7,23 @@ test_that("muLambdaStrategy returns the correct number of results", {
                                outputSize = 1, rowsFuncNodes = 5,
                                colsFuncNodes = 5, levelsBack = 2)
 
+  population[[1]]$fitness <- 5
+  population[[2]]$fitness <- 6
+  population[[3]]$fitness <- 90
+  population[[4]]$fitness <- 22
+  population[[5]]$fitness <- 150
 
 
+  functionNodeStructure <- list(rows = 5,
+                                cols = 5,
+                                levelsBack = 2,
+                                functionSet = mathOpSet())
+
+  lambda <- 4
+
+  newPopulation <- muLambdaStrategy(population, lambda,functionNodeStructure)
+
+  expect_equal(length(newPopulation), 1 + lambda)
 
 })
 
@@ -45,4 +60,63 @@ test_that("mutateFunction works correctly for nodes with no valid functionNode i
   arity <- functionSet[mutatedSolution$functionNodes[1, ]$funcID, ]$arity
 
   expect_equal(numInputs, arity)
+})
+
+test_that("mutateInput works correctly for functionNodes with one input", {
+
+  set.seed(5)
+  solution <- initPopulation(1, mathOpSet(), inputSize = 2,
+                             outputSize = 1, rowsFuncNodes = 5,
+                             colsFuncNodes = 5, levelsBack = 2)[[1]]
+
+  functionNodeStructure <- list(rows = 5,
+                                cols = 5,
+                                levelsBack = 2,
+                                functionSet = mathOpSet())
+
+  chromoID <- 23
+
+  mutation <- mutateInput(solution, chromoID, functionNodeStructure)
+
+  expect_equal(length(mutation), 1)
+
+})
+
+test_that("mutateInput works correctly for functionNodes with two inputs", {
+
+  set.seed(5)
+  solution <- initPopulation(1, mathOpSet(), inputSize = 2,
+                             outputSize = 1, rowsFuncNodes = 5,
+                             colsFuncNodes = 5, levelsBack = 2)[[1]]
+
+  functionNodeStructure <- list(rows = 5,
+                                cols = 5,
+                                levelsBack = 2,
+                                functionSet = mathOpSet())
+
+  chromoID <- 28
+
+  mutation <- mutateInput(solution, chromoID, functionNodeStructure)
+
+  expect_equal(length(mutation), 2)
+
+})
+
+test_that("mutateInput works correctly for outputNodes", {
+
+  solution <- initPopulation(1, mathOpSet(), inputSize = 3,
+                             outputSize = 1, rowsFuncNodes = 4,
+                             colsFuncNodes = 4, levelsBack = 2)[[1]]
+
+  functionNodeStructure <- list(rows = 4,
+                                cols = 4,
+                                levelsBack = 2,
+                                functionSet = mathOpSet())
+
+  chromoID <- solution$outputNodes$chromoID[1]
+
+  mutation <- mutateInput(solution, chromoID, functionNodeStructure)
+
+  expect_equal(length(mutation), 1)
+
 })
